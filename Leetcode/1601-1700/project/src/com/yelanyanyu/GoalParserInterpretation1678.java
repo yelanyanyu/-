32b27ebde9b6952f86c7e6c5b983e076263973c6
yelanyanyu <2831142384@qq.com>
2022-11-06 17:46:26 +0800

    20221106

---
+++ Leetcode/1601-1700/project/src/com/yelanyanyu/GoalParserInterpretation1678.java
@@ -0,0 +1,19 @@
+package com.yelanyanyu;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author yelanyanyu@zjxu.edu.cn
+ * @version 1.0
+ */
+public class GoalParserInterpretation1678 {
+    public String interpret(String command) {
+        return command.replace("()","o").replace("(al)","al");
+    }
+
+    @Test
+    public void Test01() {
+        String str = "G()(al)";
+        System.out.println(interpret(str));
+    }
+}
