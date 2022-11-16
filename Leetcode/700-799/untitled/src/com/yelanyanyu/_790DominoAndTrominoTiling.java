ed4e443e3e00c6caa5d1849d4570206def65d893
yelanyanyu <2831142384@qq.com>
2022-11-16 15:28:19 +0800

    _516LongestPalindromicSubsequence

---
+++ Leetcode/700-799/untitled/src/com/yelanyanyu/_790DominoAndTrominoTiling.java
@@ -0,0 +1,25 @@
+package com.yelanyanyu;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author yelanyanyu@zjxu.edu.cn
+ * @version 1.0
+ */
+public class _790DominoAndTrominoTiling {
+    //https://leetcode.cn/problems/domino-and-tromino-tiling/
+
+
+    static final int MOD = 1000000007;
+
+    public int numTilings(int n) {
+
+    }
+
+
+    @Test
+    public void Test01(){
+        System.out.println(numTilings(5));
+    }
+
+}
